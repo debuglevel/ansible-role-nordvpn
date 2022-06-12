cd7f56c3bb083c2493f5ca4d1c9ff3c2080f0026
Marc Kohaupt <debuglevel@gmail.com>
2022-06-13 00:11:36 +0200

    Fixes

--- files/routing-add-vpn-gateway.sh
+++ files/routing-add-vpn-gateway.sh
@@ -2,9 +2,9 @@
 gateway="192.168.0.1"
 interface="eth0"
 
-echo "Getting IP from OpenVPN configuration..." 
 openvpn_configuration="/etc/openvpn/nordvpn/active.ovpn"
-ip=$(cat $openvpn_configuration | grep "remote " | cut -d " " -f 2)
+echo "Getting IP from OpenVPN configuration $openvpn_configuration..." 
+ip=$(sudo cat $openvpn_configuration | grep "remote " | cut -d " " -f 2)
 echo "Got IP from OpenVPN configuration: $ip" 
 
 echo "Adding route to VPN gateway $ip via $gateway through $interface..."
